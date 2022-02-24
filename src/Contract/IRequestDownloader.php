a3a308a0aef5a3ef5a5d761c702be9fddb7a7637
Daniel Maixner <maixner@solidis.cz>
2022-02-24 17:45:53 +0100

    Interface add throws comment for IDE

--- src/Contract/IRequestDownloader.php
+++ src/Contract/IRequestDownloader.php
@@ -2,11 +2,15 @@
 
 namespace Solcloud\Http\Contract;
 
+use Solcloud\Http\Exception\HttpException;
 use Solcloud\Http\Request;
 use Solcloud\Http\Response;
 
 interface IRequestDownloader
 {
 
+    /**
+     * @throws HttpException
+     */
     public function fetchResponse(Request $request): Response;
 }
